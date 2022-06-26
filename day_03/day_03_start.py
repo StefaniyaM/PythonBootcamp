61363eb28eee5193c4df29fb962ee9a8de9461fa
StefaniyaM <84741676+StefaniyaM@users.noreply.github.com>
2022-06-26 13:53:26 +0100

    Add files via upload

---
+++ day_03/day_03_start.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height <=12:
+  print("Please, try again in a few months or years")
+else:
+  print("You are welcome for a ride")

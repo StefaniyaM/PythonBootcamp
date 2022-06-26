76cebc82ebb62425fe5c27f520cf464fcbdbc302
StefaniyaM <84741676+StefaniyaM@users.noreply.github.com>
2022-06-26 13:50:32 +0100

    Add files via upload

---
+++ day_2/day-2-2-exercise.py
@@ -0,0 +1,25 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# height = float(height)
+# weight = float(weight)
+# bmi = weight/height ** 2
+# print(int(bmi))
+
+
+''' OR '''
+
+bmi = int(weight)/float(height) ** 2
+bim_as_int = int(bmi)
+print(bim_as_int)
+
+
+
+
+
+
+

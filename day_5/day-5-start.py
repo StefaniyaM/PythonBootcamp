7df2c04785dd723cb677f5b486f704c3db12225e
StefaniyaM <84741676+StefaniyaM@users.noreply.github.com>
2022-06-26 14:21:47 +0100

    Add files via upload

---
+++ day_5/day-5-start.py
@@ -0,0 +1,16 @@
+give_me_fruit_names = input("3 fruits: ")
+fruits =give_me_fruit_names.split()
+fruits = ["Apple", "Peach", "Pear","Apple", "Peach", "Pear","Apple", "Peach", "Pear","Apple", "Peach", "Pear","Apple", "Peach", "Pear","Apple", "Peach", "Pear","Apple", "Peach", "Pear","Apple", "Peach", "Pear","Apple", "Peach", "Pear","Apple", "Peach", "Pear"]
+# print(fruits)
+
+for fruit in fruits:
+  print(fruit)
+# print(fruit)
+print(len(fruits))
+print(fruits[29])
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+  print(fruit)
+  print(fruit + " Pie")
+print(fruits)

9f32d9da7e2fdae8e47e68296d405cb41ed85683
StefaniyaM <84741676+StefaniyaM@users.noreply.github.com>
2022-06-26 13:19:39 +0100

    Add files via upload

---
+++ day_1/printingHello.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+print("Hello World!")
+print("Hello "+"Stefaniya")
+# or
+print("Hello"+" Stefaniya")
+# or
+print("Hello"+ " " +"Stefaniya") 
+
+# input function

24669af053de96a10a37a2c738377f884caca183
StefaniyaM <84741676+StefaniyaM@users.noreply.github.com>
2022-06-26 18:42:24 +0100

    Update main.py

--- day_32/main.py
+++ day_32/main.py
@@ -1,67 +1,37 @@
-# Automated Birthday Wisher
+#To run and test the code you need to update 4 places:
+# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
+# 2. Go to your email provider and make it allow less secure apps.
+# 3. Update the SMTP ADDRESS to match your email provider.
+# 4. Update birthdays.csv to contain today's month and day.
+# See the solution video in the 100 Days of Python Course for explainations.
 
-import datetime as dt
-import pandas as pd
+
+from datetime import datetime
+import pandas
 import random
+import smtplib
 
-import mailer
+MY_EMAIL = "YOUR EMAIL"
+MY_PASSWORD = "YOUR PASSWORD"
 
-BIRTHDAYS_CSV = "birthdays.csv"
-# modified these a little, so there's a custom sender name instead of "Angela"
-TEMPLATE_LIST = [
-    "./letter_templates/letter_1.txt",
-    "./letter_templates/letter_2.txt",
-    "./letter_templates/letter_3.txt",
-]
-SENDER_NAME = "Automated Birthday Wisher"
-SENDER_ADDRESS = "myemail@mydomain.zyx"
-# use the same subject line with each template
-SUBJECT_TEXT = "Happy Birthday [RECEIVER_NAME]!"
+today = datetime.now()
+today_tuple = (today.month, today.day)
 
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
-def load_birthdays():
-    """Loads the CSV file and returns the contents as a LIST."""
-    df = pd.read_csv(BIRTHDAYS_CSV)
-    # using list comprehension to create a list of dictionaries
-    # "year" isn't actually needed, but including it for completeness' sake,
-    # could be useful to calculate the person's age
-    birthdays = [{"name": row[0], "email": row[1], "year": row[2], "month": row[3], "day": row[4]} for row in df.values]
-    return birthdays
+    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
 
-
-def generate_email(details):
-    """Takes a DICT and sends an email to the provided address."""
-    # create a mail object to store all parameters in
-    mail = mailer.Email()
-    # set the sender and receiver
-    mail.set_sender(SENDER_NAME, SENDER_ADDRESS)
-    mail.set_receiver(details["name"], details["email"])
-    # set the subject, needs to be after the receiver has been set
-    mail.set_subject(SUBJECT_TEXT)
-    # choose a template
-    mail.set_message(load_template())
-    # send the email
-    mail.send()
-
-
-def load_template():
-    """Loads a random template and returns it as a STR."""
-    template = random.choice(TEMPLATE_LIST)
-    with open(template, mode="r") as f:
-        text = f.read()
-    return text
-
-
-# get today's date, including the "year" here as well
-full_datetime = dt.datetime.now()
-today = {
-    "year": full_datetime.year,
-    "month": full_datetime.month,
-    "day": full_datetime.day
-}
-# load birthdays
-birthdays_list = load_birthdays()
-for person in birthdays_list:
-    if person["month"] == today["month"] and person["day"] == today["day"]:
-        # send a mail for each match
-        generate_email(person)
